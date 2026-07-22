af27bdade34c14e5faf49e3197273740af84ddfb
visho12 <vicentepintofull2@gmail.com>
2026-07-22 17:44:18 -0400

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -1326,3 +1326,77 @@ VIDEOS_BASE = {
     "Arranque Con Un Brazo Con Mancuernas": "https://raw.githubusercontent.com/hasaneyldrm/exercises-dataset/main/videos/3888-6pTkI99.gif",
     "Curl De Bíceps De Camarero Con Mancuernas": "https://raw.githubusercontent.com/hasaneyldrm/exercises-dataset/main/videos/5201-KOpzGBL.gif",
 }
+OBJETIVOS = {
+    "Hipertrofia":    {"Reps":"6-12",  "Pausa":"1:30","RPE":"7-9",       "RM":"65-80%"},
+    "Fuerza Máxima": {"Reps":"1-5",   "Pausa":"3:00","RPE":"8-10",     "RM":"85-100%"},
+    "Resistencia":   {"Reps":"15-20+","Pausa":"0:45","RPE":"6-8",      "RM":"<60%"},
+    "Potencia":      {"Reps":"1-5",   "Pausa":"2:00","RPE":"Explosivo","RM":"30-70%"},
+}
+TIPOS_CARDIO  = ["Carrera","Bicicleta","Elíptica","Remo","Natación","HIIT","Caminata","Otro"]
+TIPOS_TEST    = ["Test Cooper (12 min)","Yo-Yo","Salto CMJ","Flexibilidad Sit&Reach",
+                 "Fuerza Relativa","1RM Estimado","Test 1km","Otro"]
+DIAS          = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"]
+GRUPOS        = ["Descanso","Pierna","Pecho/Hombro","Espalda","Glúteo",
+                 "Full Body","Torso","Brazo","Cardio"]
+TIPOS_MICROCICLO = ["Ajuste (Descarga)","Carga (Desarrollo)","Impacto (Choque)"]
+
+TABLA_BADILLO = pd.DataFrame({
+    "Zona":["Fuerza Máx","Fuerza-Hipert","Hipert Alta","Hipert Media","Resistencia"],
+    "% 1RM":["85-100%","80-85%","70-80%","60-75%","<60%"],
+    "Reps":["1-5","5-7","6-12","12-20","20+"],
+    "Descanso":["3-5 min","3 min","2 min","1-2 min","<1 min"],
+})
+GUIAS_BOMPA = pd.DataFrame({
+    "Fase":["Adaptación","Hipertrofia","Fuerza Máx","Potencia","Transición"],
+    "Intensidad":["30-60%","60-80%","85-100%","30-80%","Baja"],
+    "Reps":["12-20","6-12","1-5","1-10","Libre"],
+    "Descanso":["1-2 min","1-3 min","3-5+ min","3-5+ min","Libre"],
+})
+GUIA_TEMPO = pd.DataFrame({
+    "Objetivo":["Hipertrofia","Fuerza Máx","Potencia","Resistencia"],
+    "Tempo":["3-0-1-0","X-0-X-0","X-X-X","2-0-2-0"],
+    "Explicación":["Bajada lenta","Máx velocidad","Explosivo","Continuo"],
+})
+GUIA_DESCANSOS = pd.DataFrame({
+    "Objetivo":["Fuerza/Potencia","Hipertrofia","Resistencia"],
+    "Tiempo":["3-5+ min","60-90 seg","30-60 seg"],
+    "Por qué":["Recuperar ATP","Estrés Metabólico","Limpiar lactato"],
+})
+ESCALA_RPE = pd.DataFrame({
+    "RPE":[10,9,8,7,6],
+    "RIR":["0 (Fallo)","1","2","3","4"],
+    "Sensación":["Imposible","Podría 1 más","Podría 2 más","Podría 3 más","Calentamiento"],
+})
+ESCALA_BORG = pd.DataFrame({
+    "Nivel":["Muy Suave","Suave","Moderado","Duro","Muy Duro","Máximo"],
+    "Escala 0-10":["0-2","3","4-5","6-7","8-9","10"],
+    "Test Habla":["Cantar","Fluida","Frases","Palabras","Apenas","Sin aliento"],
+})
+GUIA_CARDIO = pd.DataFrame({
+    "Zona":["Z1 Regenerativo","Z2 Aeróbico","Z3 Umbral","Z4 VO2Max","Z5 Anaeróbico"],
+    "% VAM":["<60%","60-75%","75-90%","95-105%",">110%"],
+    "Sensación":["Muy fácil","Fácil","Duro","Muy duro","Agonía"],
+})
+TABLA_ZONAS_FCM = pd.DataFrame({
+    "Zona": [
+        "Z1 - Recuperación", 
+        "Z2 - Oxidación de Grasa", 
+        "Z3 - Aeróbica", 
+        "Z4 - Umbral Anaeróbico", 
+        "Z5 - VO2 Máx"
+    ],
+    "% FCM": [
+        "50 - 60%", 
+        "60 - 70%", 
+        "70 - 80%", 
+        "80 - 90%", 
+        "90 - 100%"
+    ],
+    "Beneficio Principal": [
+        "Recuperación activa y calentamiento", 
+        "Mayor % de uso de grasa como energía (Lipólisis)", 
+        "Mejora la resistencia cardiovascular y capacidad pulmonar", 
+        "Tolerancia al lactato (mejora umbral)", 
+        "Desarrollo de potencia y velocidad máxima"
+    ]
+})
